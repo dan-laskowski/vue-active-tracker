c3af02c757f56578eae280346df4b390d60730f4
Daniel <daniel.laskowski94@gmail.com>
2021-11-24 22:32:13 +0100

    add Navigation structure

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
-  <div>
+  <div class="min-h-full font-Poppins box-border">
+    <Navigation />
     <router-view />
   </div>
 </template>
+
+<script>
+import Navigation from "./components/Navigation.vue";
+export default {
+  components: {
+    Navigation,
+  },
+};
+</script>
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,500;0,600;0,700;1,100&display=swap");
