8bcb5dabd706fd4af0206f25abe46a21b47cdf6b
Daniel <daniel.laskowski94@gmail.com>
2021-11-26 18:27:11 +0100

    add singup a user functionality

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,35 +1,27 @@
 <template>
   <div class="max-w-screen-sm mx-auto px-4 py-10">
-    <!-- Error handling -->
-    <div v-if="errorMgs" class="mb-10 p-4 rounded-md bg-light-grey shadow-lg">
+    <!-- Error Handling -->
+    <div v-if="errorMsg" class="mb-10 p-4 rounded-md bg-light-grey shadow-lg">
       <p class="text-red-500">{{ errorMsg }}</p>
     </div>
+
     <!-- Register -->
-    <form class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg">
+    <form
+      @submit.prevent="register"
+      class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg"
+    >
       <h1 class="text-3xl text-at-light-green mb-4">Register</h1>
+
       <div class="flex flex-col mb-2">
         <label for="email" class="mb-1 text-sm text-at-light-green"
-          >email</label
+          >Email</label
         >
         <input
-          type="email"
+          type="text"
+          required
           class="p-2 text-gray-500 focus:outline-none"
           id="email"
           v-model="email"
-          required
-        />
-      </div>
-
-      <div class="flex flex-col mb-2">
-        <label for="confirmEmail" class="mb-1 text-sm text-at-light-green"
-          >Confirm email</label
-        >
-        <input
-          type="email"
-          class="p-2 text-gray-500 focus:outline-none"
-          id="confirmEmail"
-          v-model="confirmEmail"
-          required
         />
       </div>
 
@@ -39,13 +31,28 @@
         >
         <input
           type="password"
+          required
           class="p-2 text-gray-500 focus:outline-none"
           id="password"
           v-model="password"
-          required
         />
       </div>
+
+      <div class="flex flex-col mb-2">
+        <label for="confirmPassword" class="mb-1 text-sm text-at-light-green"
+          >Confirm Password</label
+        >
+        <input
+          type="password"
+          required
+          class="p-2 text-gray-500 focus:outline-none"
+          id="confirmPassword"
+          v-model="confirmPassword"
+        />
+      </div>
+
       <button
+        type="submit"
         class="
           mt-6
           py-2
@@ -61,6 +68,7 @@
       >
         Register
       </button>
+
       <router-link class="text-sm mt-6 text-center" :to="{ name: 'Login' }">
         Already have an account? <span class="text-at-light-green">Login</span>
       </router-link>
@@ -70,15 +78,41 @@
 
 <script>
 import { ref } from "vue";
+import { supabase } from "../supabase/init";
+import { useRouter } from "vue-router";
 export default {
   name: "register",
   setup() {
+    // Create data / vars
+    const router = useRouter();
     const email = ref(null);
     const password = ref(null);
     const confirmPassword = ref(null);
-    const errorMgs = ref(null);
-
-    return { email, password, confirmPassword, errorMgs };
+    const errorMsg = ref(null);
+    // Register function
+    const register = async () => {
+      if (password.value === confirmPassword.value) {
+        try {
+          const { error } = await supabase.auth.signUp({
+            email: email.value,
+            password: password.value,
+          });
+          if (error) throw error;
+          router.push({ name: "Login" });
+        } catch (error) {
+          errorMsg.value = error.message;
+          setTimeout(() => {
+            errorMsg.value = null;
+          }, 5000);
+        }
+        return;
+      }
+      errorMsg.value = "Error: Passwords do not match";
+      setTimeout(() => {
+        errorMsg.value = null;
+      }, 5000);
+    };
+    return { email, password, confirmPassword, errorMsg, register };
   },
 };
 </script>
