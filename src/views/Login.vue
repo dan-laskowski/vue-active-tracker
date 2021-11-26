1ec941564937fb871ab9e25e738cf6a2b48cae71
Daniel <daniel.laskowski94@gmail.com>
2021-11-26 19:06:28 +0100

    add login a user functionality

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="max-w-screen-sm mx-auto px-4 py-10">
     <!-- Error handling -->
-    <div v-if="errorMgs" class="mb-10 p-4 rounded-md bg-light-grey shadow-lg">
+    <div v-if="errorMsg" class="mb-10 p-4 rounded-md bg-light-grey shadow-lg">
       <p class="text-red-500">{{ errorMsg }}</p>
     </div>
     <!-- Login -->
-    <form class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg">
+    <form
+      @submit.prevent="login"
+      class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg"
+    >
       <h1 class="text-3xl text-at-light-green mb-4">Login</h1>
       <div class="flex flex-col mb-2">
         <label for="email" class="mb-1 text-sm text-at-light-green"
@@ -33,6 +36,7 @@
         />
       </div>
       <button
+        type="submit"
         class="
           mt-6
           py-2
@@ -58,14 +62,34 @@
 
 <script>
 import { ref } from "vue";
+import { supabase } from "../supabase/init";
+import { useRouter } from "vue-router";
 export default {
   name: "login",
   setup() {
+    const router = useRouter();
     const email = ref(null);
     const password = ref(null);
-    const errorMgs = ref(null);
+    const errorMsg = ref(null);
 
-    return { email, password, errorMgs };
+    //Login function
+    const login = async () => {
+      try {
+        const { error } = await supabase.auth.signIn({
+          email: email.value,
+          password: password.value,
+        });
+        if (error) throw error;
+        router.push({ name: "Home" });
+      } catch (error) {
+        errorMsg.value = `Error: ${error.message}`;
+        setTimeout(() => {
+          errorMsg.value = null;
+        }, 5000);
+      }
+    };
+
+    return { email, password, errorMsg, login };
   },
 };
 </script>
