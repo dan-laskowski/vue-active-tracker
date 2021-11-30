b9d9b69942ee9008a0626b114e890abf952f8b93
Daniel <daniel.laskowski94@gmail.com>
2021-11-30 17:54:31 +0100

    add page titles to browser

--- src/router/index.js
+++ src/router/index.js
@@ -10,32 +10,52 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+    meta: {
+      title: "Your trainings",
+    },
   },
   {
     path: "/login",
     name: "Login",
     component: Login,
+    meta: {
+      title: "Login to your account",
+    },
   },
   {
     path: "/register",
     name: "Register",
     component: Register,
+    meta: {
+      title: "Register your account",
+    },
   },
   {
     path: "/create",
     name: "Create",
     component: Create,
+    meta: {
+      title: "Create new training",
+    },
   },
   {
     path: "/view-workout/:workoutId",
     name: "ViewWorkout",
     component: ViewWorkout,
+    meta: {
+      title: "View workout",
+    },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} | Activity Tracker`;
+  next();
 });
 
 export default router;
