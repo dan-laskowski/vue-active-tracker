4c97423cf81f9e40d261dea3e3df6288f7fb4146
Daniel <daniel.laskowski94@gmail.com>
2021-11-30 16:39:51 +0100

    add workout exercises view

--- src/views/ViewWorkout.vue
+++ src/views/ViewWorkout.vue
@@ -105,6 +105,191 @@
         </div>
       </div>
     </div>
+    <!-- Exercises information -->
+    <div
+      class="
+        mt-10
+        p-8
+        rounded-md
+        flex flex-col
+        item-center
+        bg-light-grey
+        shadow-md
+      "
+    >
+      <!-- Strength training-->
+      <div
+        v-if="data.workoutType === 'strength'"
+        class="flex flex-col gap-y-4 w-full"
+      >
+        <div
+          class="flex flex-col gap-x-6 gap-y-2 relative sm:flex-row"
+          v-for="(item, index) in data.exercises"
+          :key="index"
+        >
+          <div class="flex flex-2 flex-col md:w-1/3">
+            <label for="exercise-name" class="mb-1 text-sm text-at-light-green">
+              Exercise</label
+            >
+            <input
+              id="exercise-name"
+              type="text"
+              v-if="edit"
+              class="p-2 w-full text-gray-500 focus:outline-none"
+              v-model="item.exercise"
+            />
+            <p v-else>{{ item.exercise }}</p>
+          </div>
+          <div class="flex flex-1 flex-col">
+            <label for="sets" class="mb-1 text-sm text-at-light-green">
+              Sets</label
+            >
+            <input
+              id="sets"
+              type="text"
+              v-if="edit"
+              class="p-2 w-full text-gray-500 focus:outline-none"
+              v-model="item.sets"
+            />
+            <p v-else>{{ item.sets }}</p>
+          </div>
+          <div class="flex flex-1 flex-col">
+            <label for="reps" class="mb-1 text-sm text-at-light-green">
+              Reps</label
+            >
+            <input
+              id="reps"
+              type="text"
+              v-if="edit"
+              class="p-2 w-full text-gray-500 focus:outline-none"
+              v-model="item.reps"
+            />
+            <p v-else>{{ item.reps }}</p>
+          </div>
+          <div class="flex flex-1 flex-col">
+            <label for="weight" class="mb-1 text-sm text-at-light-green">
+              Weight</label
+            >
+            <input
+              id="weight"
+              type="text"
+              v-if="edit"
+              class="p-2 w-full text-gray-500 focus:outline-none"
+              v-model="item.weight"
+            />
+            <p v-else>{{ item.weight }}</p>
+          </div>
+          <img
+            v-if="edit"
+            class="absolute h-4 w-auto -left-5 cursor-pointer"
+            src="../assets/images/trash-light-green.png"
+            alt=""
+          />
+        </div>
+        <button
+          class="
+            mt-6
+            py-2
+            px-6
+            rounded-sm
+            self-start
+            text-sm text-white
+            bg-at-light-green
+            duration-200
+            border-solid border-2 border-transparent
+            hover:border-at-light-green hover:bg-white hover:text-at-light-green
+          "
+        >
+          Add exercise
+        </button>
+      </div>
+
+      <!-- Cardio training-->
+      <div v-else class="flex flex-col gap-y-4 w-full">
+        <div
+          class="flex flex-col gap-x-6 gap-y-2 relative sm:flex-row"
+          v-for="(item, index) in data.exercises"
+          :key="index"
+        >
+          <div class="flex flex-2 flex-col md:w-1/3">
+            <label for="cardio-type" class="mb-1 text-sm text-at-light-green">
+              Type</label
+            >
+            <select
+              class="p-2 w-full text-gray-500 focus:outline-none"
+              v-if="edit"
+              id="cardio-type"
+            >
+              <option value="#" disabled>Select type</option>
+              <option value="run">Run</option>
+              <option value="walk">Walk</option>
+            </select>
+            <p v-else>{{ item.cardioType }}</p>
+          </div>
+          <div class="flex flex-1 flex-col">
+            <label for="distance" class="mb-1 text-sm text-at-light-green">
+              Distance</label
+            >
+            <input
+              id="distance"
+              type="text"
+              v-if="edit"
+              class="p-2 w-full text-gray-500 focus:outline-none"
+              v-model="item.distance"
+            />
+            <p v-else>{{ item.distance }}</p>
+          </div>
+          <div class="flex flex-1 flex-col">
+            <label for="duration" class="mb-1 text-sm text-at-light-green">
+              Duration</label
+            >
+            <input
+              id="duration"
+              type="text"
+              v-if="edit"
+              class="p-2 w-full text-gray-500 focus:outline-none"
+              v-model="item.duration"
+            />
+            <p v-else>{{ item.duration }}</p>
+          </div>
+          <div class="flex flex-1 flex-col">
+            <label for="pace" class="mb-1 text-sm text-at-light-green">
+              Pace</label
+            >
+            <input
+              id="pace"
+              type="text"
+              v-if="edit"
+              class="p-2 w-full text-gray-500 focus:outline-none"
+              v-model="item.pace"
+            />
+            <p v-else>{{ item.pace }}</p>
+          </div>
+          <img
+            v-if="edit"
+            class="absolute h-4 w-auto -left-5 cursor-pointer"
+            src="../assets/images/trash-light-green.png"
+            alt=""
+          />
+        </div>
+        <button
+          class="
+            mt-6
+            py-2
+            px-6
+            rounded-sm
+            self-start
+            text-sm text-white
+            bg-at-light-green
+            duration-200
+            border-solid border-2 border-transparent
+            hover:border-at-light-green hover:bg-white hover:text-at-light-green
+          "
+        >
+          Add exercise
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
